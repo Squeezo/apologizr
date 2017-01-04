c01067d10240b711aae0b1995b8c793b0025aba6
Squeezo <squeezemccoy@gmail.com>
2017-01-03 17:49:16 -0800

    begin basic tweetstream support

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 require('dotenv').config();
-const express = require('express'),
-      app = express(),
-      post = require('./routes/post'),
+const express = require('express')
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const twitterClient = require('./twitterClient'),
       expressValidator = require('express-validator'),
-      bodyParser = require('body-parser');
-      
+      bodyParser = require('body-parser'),
+      ip = require('ip');
+
+const post = require('./routes/post');
+var socket;
+
+app.use(express.static(__dirname + '/public'));
 app.engine('pug', require('pug').__express)
 app.set('views', __dirname + '/views')
 app.set('view engine', 'pug')
@@ -13,25 +20,61 @@ app.disable('x-powered-by')
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator())
 
-const server = app.listen(80, (err, response) => {
+server.listen(80, (err, response) => {
   if (err) console.log(err.stack)
   const port = server.address().port
   console.log('apologizr listening at port %s', port)
 })
 
-
 app.use(post)
+//app.use(search)
+
+
+app.get('/search', (req, res) => {
+  res.render('search', {label: 'What are you looking for?', search:'Search', siteIP : req.protocol + '://' + ip.address()})
+
+})
+
+app.post('/search', (req, res) => {
+
+  let searchFor = req.body.searchFor;
+  req.sanitize('searchFor').escape();
+  console.log('santized search query', req.body.searchFor)
+
+  io.on('connection', (sock) => {
+    console.log('Connected')
+    socket = sock;
+  })
+
+
+
+  twitterClient.stream('statuses/filter', {track: searchFor}, (stream) => {
+
+    stream.on('data', (tweets) => {
+      console.log('got new tweets')
+      if(!socket.emit) {
+        console.log('No socket connection')
+      } else {
+        socket.emit('results',tweets)
+      }
+    });
+    
+    stream.on('error', (error) => {
+      console.log(error);
+      
+    });
+    
+    return res.render('search', {label: 'What are you looking for?', search:'Search'})
+
+  });
+
+})
+
 
 app.on('listening', () => { 
   console.log('Initializing Twitter instance')
   const client = require('twitterClient');
   if(!client) { 
     console.log('Error connecting to Twitter.')
-    server.close();
   }
 })
-
-exports.close = () => {
-  console.log('apologizr exiting.')
-  server.close()
-}
